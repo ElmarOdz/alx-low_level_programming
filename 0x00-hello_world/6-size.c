63ce950104256f8310eae6e0b1eb3db2fbaa029d
ElmarOdz <mlexie92@gmail.com>
2022-09-08 01:46:10 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * Warnings are allowed
+ * main - Entry point
  *
  *
- * Your program should return 0
  *
- **/
-
+ * Return: always 0
+ *
+ */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
