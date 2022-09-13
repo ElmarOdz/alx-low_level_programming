1cecf5b0f090f0f4eea2ee620e9935c8e1172f5f
ElmarOdz <mlexie92@gmail.com>
2022-09-13 02:51:20 -0700

    main header update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+
+#include "main.h"
 
 /**
  * main - a program that prints _putchar, followed by a new line
@@ -8,10 +9,19 @@
  *
  **/
 
-int main()
+int main(void)
 {
+	int _putchar(char c);
 
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
