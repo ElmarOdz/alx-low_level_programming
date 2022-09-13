821a2892f84f91a69545646831316c2926e7c163
ElmarOdz <mlexie92@gmail.com>
2022-09-13 03:32:25 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: 0
+ *
+ **/
+
+void print_alphabet(void)
+{
+	char lettstart;
+
+	lettstart = 'a';
+
+	while (lettstart <= 'z')
+	{
+		_putchar(lettstart);
+		lettstart++;
+	}
+	_putchar('\n');
+}
