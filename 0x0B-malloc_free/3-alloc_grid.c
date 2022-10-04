ca426b334d4c53502b4c4caf59e48a8d9be1d8ce
ElmarOdz <mlexie92@gmail.com>
2022-10-04 00:35:59 -0700

    returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,29 +12,37 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **twoD;
-	int hgt_index, wid_index;
+	int **array;
+	int i = 0, j;
 
-	if (width <= 0 || height <= 0)
+	if (width == 0 || height == 0)
 		return (NULL);
-		twoD = malloc(sizeof(int *) * height);
-	if (twoD == NULL)
-		return (NULL);
-	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	array = (int **) malloc(sizeof(int *) * height);
+	if (array != NULL)
 	{
-		twoD[hgt_index] = malloc(sizeof(int) * width);
-		if (twoD[hgt_index] == NULL)
+		for (; i < height; i++)
 		{
-			for (; hgt_index >= 0; hgt_index--)
-				free(twoD[hgt_index]);
-			free(twoD);
-			return (NULL);
+			array[i] = (int *) malloc(sizeof(int) * width);
+			if (array[i] != NULL)
+			{
+				for (j = 0; j < width; j++)
+					array[i][j] = 0;
+			}
+			else
+			{
+				while (i >= 0)
+				{
+					free(array[i]);
+					i--;
+				}
+				free(array);
+				return (NULL);
+			}
 		}
+		return (array);
 	}
-	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	else
 	{
-		for (wid_index = 0; wid_index < width; wid_index++)
-			twoD[hgt_index][wid_index] = 0;
+		return (NULL);
 	}
-	return (twoD);
 }
