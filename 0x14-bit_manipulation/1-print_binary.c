6e165cb4bef10ebe0a0269718f8e6a5d3bcb1bf5
ElmarOdz <mlexie92@gmail.com>
2022-10-27 08:55:43 -0700

    a function that prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 63)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	return ((n >> index) & 1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
 }
