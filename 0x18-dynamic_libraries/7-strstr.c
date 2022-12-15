2bc0bf303b64d6ec280f1981f7c9d89b07cd01da
ElmarOdz <mlexie92@gmail.com>
2022-12-15 13:20:19 -0800

    update lib

--- 0x18-dynamic_libraries/7-strstr.c
+++ 0x18-dynamic_libraries/7-strstr.c
@@ -5,10 +5,7 @@
  * _strstr -  a function that locates a substring.
  * @s: an input string to search in
  * @c: an input string to locate into string haystack
- * Return:  a pointer to the beginning of the located substring,
- * or NULL if the substring is not found.
- */
+ * Return:  a pointer to the beginning of the located substring
+ **/
+
 char *_strchr(char *s, char c);
-{
-  
-}
