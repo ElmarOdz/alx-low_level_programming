0b12a445b61c2cc6d933c8f4a098ab923856790c
ElmarOdz <110916781+ElmarOdz@users.noreply.github.com>
2022-12-16 00:06:55 +0300

    Create 7-strstr.c

---
+++ 0x18-dynamic_libraries/7-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr -  a function that locates a substring.
+ * @s: an input string to search in
+ * @c: an input string to locate into string haystack
+ * Return:  a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strchr(char *s, char c);
+{
+  
+}
